090d57538ca6dd61506b0b9426c2106276fb0011
Gopal Mathur <mathuyrgopal@gmail.com>
2025-12-21 15:35:04 +0530

    added exhaust pressure and temperature private variables

--- include/ice_engine.hpp
+++ include/ice_engine.hpp
@@ -13,12 +13,14 @@ public:
 
 private:
   // state
-  double angular_velocity;            // rad/s
-  double throttle;                    // 0..1
-  double torque_output;               // Nm
-  double intake_manifold_pressure;    // Pa
-  double intake_manifold_temperature; // K
-  double load_torque;                 // Nm
+  double angular_velocity;         // rad/s
+  double throttle;                 // 0..1
+  double torque_output;            // Nm
+  double intake_manifold_pressure; // Pa
+  double exhaust_manifold_pressure; // Pa
+  double intake_manifold_temperature;  // K
+  double exhaust_manifold_temperature; // K
+  double load_torque;                  // Nm
 
   // friction model parameters
   double frictionA;
